4b7e41cd23509edc32b41526670bfb4ace2a982f
Atomute <73423300+Atomute@users.noreply.github.com>
2023-07-23 08:58:51 +0000

    finish part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,48 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  arr.each { |n| total = total+n}
+return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  total = 0
+  sortedarr = arr.sort.reverse
+
+  sortedarr.slice(0,2).each { |n| total += n}
+
+return total
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  len = arr.length()
+  (0..len-1).each do |i|
+    (i+1..len).each do |j|
+      return true if arr.at(i)+arr.at(j)==n
+    end
+  end
+return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  vowels = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z','V']
+  vowels.each do |i|
+    if s[0] == i
+      return true
+    end
+  end
+return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  
 end
 
 # Part 3
